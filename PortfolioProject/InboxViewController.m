a1110206c0a0a87f490468b3d7480fb53b26d241
Calvin Nisbet <cnisb044@uottawa.ca>
2015-03-04 01:49:07 -0800

    2AM

--- PortfolioProject/InboxViewController.m
+++ PortfolioProject/InboxViewController.m
@@ -1,13 +1,13 @@
-//
+
 //  InboxViewController.m
 //  PortfolioProject
-//
+
 //  Created by Calvin Nisbet on 2015-02-23.
 //  Copyright (c) 2015 Calvin Nisbet. All rights reserved.
-//
+
 
 #import "InboxViewController.h"
-
+#import "ImageViewController.h"
 #import <Parse/Parse.h>
 
 @implementation InboxViewController
@@ -25,7 +25,26 @@
     [self performSegueWithIdentifier:@"showLogin" sender:self];
 }
 
-
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    //Querying for all inbox messages.
+    PFQuery *query = [PFQuery queryWithClassName:@"Messages"];
+    [query whereKey:@"recipientsIds" equalTo:[[PFUser currentUser] objectId]];
+    [query orderByDescending:@"createdAt"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (error) {
+            NSLog(@"Error: %@ %@", error, [error userInfo]);
+        }
+        else
+        {
+        //We found messages!
+        self.messages = objects;
+            [self.tableView reloadData];
+            NSLog(@"Retrieved: %lu", (unsigned long)[self.messages count]);
+        }
+    }];
+}
 
 - (IBAction)logout:(id)sender {
     
@@ -37,8 +56,52 @@
 {
     if ([segue.identifier isEqualToString:@"showLogin"]) {
         [segue.destinationViewController setHidesBottomBarWhenPushed:YES];
+    } else ([segue.identifier isEqualToString:@"showImage"]);
+    {
+        [segue.destinationViewController setHidesBottomBarWhenPushed:YES];
+        ImageViewController *imageViewController = (ImageViewController*)segue.destinationViewController;
+        imageViewController.message = self.selectedMessage;
     }
 }
 
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.messages count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    PFObject *message = [self.messages objectAtIndex:indexPath.row];
+    cell.textLabel.text = [message objectForKey:@"senderName"];
+    NSString *fileType = [message objectForKey:@"fileType"];
+    if ([fileType isEqualToString:@"image"]) {
+        cell.imageView.image = [UIImage imageNamed:@"icon_image"];
+    } else
+    {
+        cell.imageView.image = [UIImage imageNamed:@"icon_video"];
+    }
+    return cell;
+}
+
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    self.selectedMessage = [self.messages objectAtIndex:indexPath.row];
+    NSString *fileType = [self.selectedMessage objectForKey:@"fileType"];
+        if ([fileType isEqualToString:@"image"]) {
+        [self performSegueWithIdentifier:@"showImage" sender:self];
+    } else
+    {
+        [self performSegueWithIdentifier:@"showVideo" sender:self];
+      
+    }
+
+}
 
 @end
